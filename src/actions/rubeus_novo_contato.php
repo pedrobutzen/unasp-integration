9d073f86c3d73a155c4f07fb6ab4889f08ebf7eb
Pedro Butzen <pedrobutzen@outlook.com>
2019-10-09 14:54:40 -0300

    Incrementando evento de novo contato no rubeus

--- src/actions/rubeus_novo_contato.php
+++ src/actions/rubeus_novo_contato.php
@@ -27,12 +27,25 @@ class rubeus_novo_contato extends Evento {
     }
 
     public function cast_values(array $data) {
+        $data = array_merge([
+            "cpf" => null,
+            "sexo" => null,
+            "estado" => null,
+            "cidade" => null,
+        ], $data);
+
         return [
             'codigo' => $data['codigo'],
             'nome' => $data['nome'],
             'dataNascimento' => $data['nascimento'],
             'telefonePrincipal' => $data['telefone'],
             'emailPrincipal' => $data['email'],
+            'cpf' => $data['cpf'],
+            'sexo' => $data['sexo'],
+            'estadoCidade' => [
+                'cidade' => $data['cidade'],
+                'estado' => $data['estado'],
+            ],
         ];
     }
 }
